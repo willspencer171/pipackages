2e40930d676a4e9b86f529a0c3e236aa8fd92e4d
Will Spencer <willspencer171@gmail.com>
2023-03-30 16:19:46 +0100

    updated install.py

--- install.py
+++ install.py
@@ -2,4 +2,6 @@ import subprocess
 import pathlib
 
 # this will execute the installation of requirement packages
-subprocess.call(["pip", "install", "-r", str(pathlib.Path(__file__).parent.resolve()) + "/requirements.txt"])
+#subprocess.call(["pip", "install", "-r", str(pathlib.Path(__file__).parent.resolve()) + "/requirements.txt"])
+
+print(subprocess.getoutput("lsvirtualenv").split("\n")[3:-1])
